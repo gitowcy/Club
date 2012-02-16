ce3f2241016a5502783f50a931d62ba2bd84ab8c
Maciej Palinski <twojeimie@wp.pl>
2012-02-16 12:55:30 +0100

    Trolololo

---
+++ src/club/Club.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package club;
+
+/**
+ *
+ * @author Palin, Muszla, Tempy
+ */
+public class Club {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+}
