5f1762f7ea96ddfb4c83cec25dd8e7050c70f9cf
Maciej Palinski <twojeimie@wp.pl>
2012-02-16 15:47:42 +0100

    Repaired club.java

--- src/club/Club.java
+++ src/club/Club.java
@@ -8,17 +8,16 @@ package club;
  *
 
  * @author Palin, Muszla, Ostry 456
-public class Club {
+
 
   /**
    * @param args the command line arguments
    */
+public class Club {
   public static void main(String[] args) {
-    // TODO code application logic here
-<<<<<<< HEAD
-=======
+
     MainFrame mf = new MainFrame();
     mf.setVisible(true);
->>>>>>> 8d17db4cd943c802296fed2b51ebd77890bc197a
+
   }
 }
